b188de3e9850c67e236efa45b41a1083e8f7008d
Matthias Claeys <mfbclaeys@login03.delftblue.tudelft.nl>
2026-02-10 17:06:36 +0100

    test commit

--- gpu_batch.sh
+++ gpu_batch.sh
@@ -30,6 +30,6 @@ echo "Finished Loading Modules"
 
 echo "Running Lux_AE_train.jl"
 
-srun julia --project project/AE/Lux_AE_train.jl
+#srun julia --project project/AE/Lux_AE_train.jl
 
 echo "Finished at $(date)"
