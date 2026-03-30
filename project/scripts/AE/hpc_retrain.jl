f316c8a45b9275d750db3aab4cc1435f82ed09d6
claeys01 <matthias.claeys2002@gmail.com>
2026-03-30 12:31:57 +0200

    updated hpc retrain script

--- project/scripts/AE/hpc_retrain.jl
+++ project/scripts/AE/hpc_retrain.jl
@@ -28,7 +28,7 @@ function main()
     end
 
     node_path = joinpath(root_path, "data/saved_models/NODE/16/RE2500/E1000_curldiv_MS_Adam_250/node_params.jld2")
-    AE_path = joinpath(root_path, "scratch/mfbclaeys/data/saved_models/u/Lux/256h_16l/RE2500/2e8/Feb12-1530__E1000_HW256x256_C4to2_nc6_nd2_z16_C8_lr0p001_wd0p0009_bs16_NY_LL1_Tl0p0471/checkpoint.jld2")
+    AE_path = joinpath(root_path, "data/saved_models/u/Lux/256h_16l/RE2500/2e8/Feb12-1530__E1000_HW256x256_C4to2_nc6_nd2_z16_C8_lr0p001_wd0p0009_bs16_NY_LL1_Tl0p0471/checkpoint.jld2")
     # aenode = AENODE(AE_path, node_path)
     checkpoint = JLD2.load(AE_path)
     args_dict = checkpoint["args"]
