d4c765330d2a47d880eaade68074a7772aec0d48
claeys01 <matthias.claeys2002@gmail.com>
2026-04-23 14:59:19 +0200

    bug fixes

--- project/scripts/Inline/inline_noload.jl
+++ project/scripts/Inline/inline_noload.jl
@@ -28,7 +28,7 @@ if is_hpc()
         node_iters = 500,
         node_retrain_iters = 500,
         n_switch = 150,
-        save_interval = 1, # needs to be fixed still, 
+        save_interval = 0.25, # needs to be fixed still, 
     )
 end
 
@@ -65,6 +65,7 @@ ae_bundle, AE_path = train_AE(ae_args; return_path=true)
 ae_elapsed = round((time() - ae_start) / 60; digits=1)
 @info "AE initial training complete" elapsed_min=ae_elapsed checkpoint=AE_path
 
+ae_args.simdata_ram = nothing   # release the simdata ref
 normalizer = load_normalizer(AE_path)
 
 # ================================ Step 2: Train NODE ================================
@@ -106,16 +107,14 @@ if hs.retrain_needed
     println("continueing to run simulation without AENODE")
 
     simdata = run_warmup!(hs, sim_time(hs.sim) + 20; simdata=simdata, save_path=simdata_path)
-    simdata = run_warmup!(hs, sim_time(hs.sim) + 20; simdata=simdata, save_path=simdata_path)
 
     # ================================ Step 3: Retrain AE ================================
     ae_retrain_start = time()
-    ae_args = LuxArgs(
+    ae_retrain_args = LuxArgs(
         η = 2e-4,
         epochs=params.ae_retrain_epochs, 
         λdiv=Float64(div), 
         λcurl=Float64(curl),
-        t_training=simdata.time[end] * 0.8 ,
         t_training=simdata.time[end] * 0.8 ,
         retrain=true,
         checkpoint_path=AE_path,
@@ -123,20 +122,18 @@ if hs.retrain_needed
         simdata_ram=simdata,
     )
     
-    ae_bundle, AE_path = train_AE(ae_args; return_path=true)
-    normalizer = load_normalizer(AE_path)
+    ae_retrain_bundle, AE_retrain_path = train_AE(ae_retrain_args; return_path=true)
+    retrain_normalizer = load_normalizer(AE_retrain_path)
     ae_retrain_elapsed = round((time() - ae_retrain_start) / 60; digits=1)
     @info "AE retraining complete" elapsed_min=ae_retrain_elapsed checkpoint=AE_path
 
     # ================================ Step 4: Retrain NODE ================================
 
     @info "── Step 4/4: Retraining Neural ODE ──"
-    ae_bundle = cpu_device()(ae_bundle)
+    ae_retrain_bundle = cpu_device()(ae_retrain_bundle)
     GC.gc()
     node_retrain_start = time()
-
-    node_retrain_path = train_NODE(
-        NodeArgs(
+    node_retrain_args = NodeArgs(
             extrapolate = false,
             latent_dim = ae_args.latent_dim,
             η = 0.005,              # lower LR for fine-tuning
@@ -148,22 +145,28 @@ if hs.retrain_needed
             multiple_shooting = true,
             use_gpu = false, 
             node_checkpoint = node_path,
-        );
-        ae_bundle = ae_bundle,
-        normalizer = normalizer, ae_args = ae_args,
+        )
+        
+    node_retrain, node_retrain_path = train_NODE(node_retrain_args;
+        ae_bundle = ae_retrain_bundle,
+        normalizer = retrain_normalizer, ae_args = ae_retrain_args,
     )
     node_retrain_elapsed = round((time() - node_retrain_start) / 60; digits=1)
     @info "NODE retraining complete" elapsed_min=node_retrain_elapsed node_path=node_retrain_path
 
 
-    hs.aenode = AENODE(ae_bundle, node, ae_args, node_args, normalizer; verbose=true)
-    hs.AE_path = AE_path
-    hs.node_path = node_path
+    hs.aenode = AENODE(ae_retrain_bundle, node_retrain, ae_retrain_args, node_retrain_args, retrain_normalizer; verbose=true)
+    hs.AE_path = AE_retrain_path
+    hs.node_path = node_retrain_path
     hs.retrain_needed = false
     hs.step = 0
     
     push!(hs.mode_log, (t_start=sim_time(hs.sim), t_end=sim_time(hs.sim), mode="Restarted"))
     run_hybrid!(hs)
+
+    if sim_time(hs.sim) < params.t_accel_end
+        simdata = run_warmup!(hs, params.t_accel_end; simdata=simdata, save_path=simdata_path)
+    end
 end
 
 save_results(hs)
