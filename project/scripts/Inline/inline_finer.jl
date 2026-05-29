bfcac8fa02b92d2d8cea03480ba9b32a2a9f4751
claeys01 <matthias.claeys2002@gmail.com>
2026-05-29 09:45:28 +0200

    more changes for inline finer

--- project/scripts/Inline/inline_finer.jl
+++ project/scripts/Inline/inline_finer.jl
@@ -41,7 +41,7 @@ simdata_path = joinpath(savedir, "U_inline_finer.jld2")
 # u₀ = load_u0("data/datasets/RE2500/2e8/U_128_full_u0.jld2")
 
 sim = circle_shedding_biot(n=2^9, m=2^9; mem=Array, perturb=false)
-u₀ = load_u0(joinpath(root_path, "data/initial_fields/u0_biot_n512_t50.jld2"))
+u₀ = load_u0(joinpath(root_path, "data/initial_fields/Ru0_biot_n512_t50.jld2"))
 
 hs = HybridState(sim, nothing, params, savedir, nothing, nothing)
 
@@ -67,6 +67,7 @@ ae_args = LuxArgs(
         input_dim = (2^9, 2^9, 4),   # flow field size with μ₀ concatenated
         output_dim = (2^9, 2^9, 2),  # size of reconstructed RHS field
         n_conv = 8,
+        batch_size=4,
         n_dense= 2,
         t_training=params.t_train,
         full_data_path=simdata_path, 
