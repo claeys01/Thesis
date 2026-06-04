c413f8781d38b53d22c98d5786b1c96d3110a68d
claeys01 <matthias.claeys2002@gmail.com>
2026-06-04 10:51:46 +0200

    changed label name

--- project/scripts/Inline/inline_sweep_final.jl
+++ project/scripts/Inline/inline_sweep_final.jl
@@ -34,7 +34,7 @@ configs = [
     (name="aeEpochs_250",          ae_epochs=250, ae_batch_size=16, latent_dim=16, λdiv=100.0,  λcurl=100.0),
     (name="aeEpochs_750",          ae_epochs=750, ae_batch_size=16, latent_dim=16, λdiv=100.0,  λcurl=100.0),
     (name="batch_4",               ae_epochs=500, ae_batch_size=4,  latent_dim=16, λdiv=100.0,  λcurl=100.0),
-    (name="batch_18",              ae_epochs=500, ae_batch_size=8, latent_dim=16, λdiv=100.0,  λcurl=100.0),
+    (name="batch_8",               ae_epochs=500, ae_batch_size=8,  latent_dim=16, λdiv=100.0,  λcurl=100.0),
     (name="latent_8",              ae_epochs=500, ae_batch_size=16, latent_dim=8,  λdiv=100.0,  λcurl=100.0),
     (name="latent_32",             ae_epochs=500, ae_batch_size=16, latent_dim=32, λdiv=100.0,  λcurl=100.0),
     (name="phys_div0_curl0",       ae_epochs=500, ae_batch_size=16, latent_dim=16, λdiv=0.0,    λcurl=0.0),
